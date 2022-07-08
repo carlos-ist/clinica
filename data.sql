01e7a27f5326110d966d148739806eb6db3879d3
jose-ist <carlos.work@outlook.com>
2022-07-08 18:15:54 -0300

    data.sql

---
+++ data.sql
@@ -0,0 +1,19 @@
+INSERT INTO PACIENTE (id, Nome) values (1,'Roberto');
+INSERT INTO PACIENTE (id, Nome) values (2,'Renato');
+INSERT INTO PACIENTE (id, Nome) values (3,'Ronaldo');
+INSERT INTO PACIENTE (id, Nome) values (4,'Robson');
+INSERT INTO PACIENTE (id, Nome) values (5,'Rui');
+
+INSERT INTO MEDICO (id, Nome) values (1,'Rafael');
+INSERT INTO MEDICO (id, Nome) values (2,'Michelangelo');
+INSERT INTO MEDICO (id, Nome) values (3,'Leonardo');
+INSERT INTO MEDICO (id, Nome) values (4,'Donatelo');
+INSERT INTO MEDICO (id, Nome) values (5,'Me.Splinter');
+
+/*
+INSERT INTO CONSULTA (id, id.PACIENTE, id.MEDICO, dataHora) values (1,'Rafael');
+INSERT INTO CONSULTA (id, id.PACIENTE, id.MEDICO, dataHora) values (2,'Rafael');
+INSERT INTO CONSULTA (id, id.PACIENTE, id.MEDICO, dataHora) values (3,'Rafael');
+INSERT INTO CONSULTA (id, id.PACIENTE, id.MEDICO, dataHora) (4,'Michelangelo');
+INSERT INTO CONSULTA (id, id.PACIENTE, id.MEDICO, dataHora) values (5,'Donatelo');
+*/
